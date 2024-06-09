e56dbc6dae048c365a9aae04b99773b387f6f187
Naol Ketema <naolketema55@gmail.com>
2024-06-09 16:49:46 +0300

    simple user crud done

--- db.go
+++ db.go
@@ -1,2 +1,21 @@
 package main
 
+import (
+	"log"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func initDatabase() *gorm.DB {
+
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+    if err != nil{
+        log.Fatalf("failed to connect to db")
+    }
+
+    db.AutoMigrate(&User{})
+
+    return db
+
+}
--- main.go
+++ main.go
@@ -1,23 +1,89 @@
 package main
 
 import (
+	"log"
+	"strconv"
+
 	"github.com/gofiber/fiber/v2"
 )
 
 func main(){
 
 	app := fiber.New()
+	db := initDatabase()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, this is a GET API")
-	 })
+	app.Post("/users", func(c *fiber.Ctx) error {
+		user := new(User)
 
-	 app.Post("/", func(c *fiber.Ctx) error{
-		return c.SendString("hello this is a post request")
-	 })
+		if err := c.BodyParser(user); err != nil{
+			return c.Status(400).SendString(err.Error())
+		}
+		db.Create(&user)
+		return c.JSON(user)
+	})
+
+	app.Get("/users", func(c *fiber.Ctx) error {
+		var users []User
+		db.Find(&users)
+		return c.JSON(users)
+	})
 
 
+	app.Get("/users/:id", func(c *fiber.Ctx) error {
+		id := c.Params("id")
+
+		var user User
+
+		if err := db.First(&user, id); err.Error != nil {
+			return c.Status(400).SendString("user not found with this id")
+		}
+
+		return c.JSON(user)
+	})
+
+	app.Delete("/users/:id", func(c *fiber.Ctx) error {
+		id := c.Params("id")
+
+		var user User
+
+		if err := db.First(&user, id); err.Error != nil{
+			return c.Status(400).SendString("user with this id doesnt exist")
+		}
+		db.Delete(&user)
+		return c.SendString("user deleted successfully")
+	})
+
+	
+
+	app.Put("/users/:id", func(c *fiber.Ctx) error {
+		idStr := c.Params("id")
+
+		id, err := strconv.Atoi(idStr)
+		if err != nil {
+		        log.Printf("Invalid user ID: %s", idStr)
+		        return c.Status(400).SendString("Invalid user ID")
+		    }
+		
+		var user User
+
+		if err := db.First(&user, id); err.Error != nil{
+			log.Printf("Error finding user: %v", err)
+			return c.Status(404).SendString("user not found " )
+		}
+
+		if err := c.BodyParser(&user); err != nil{
+			return c.Status(400).SendString(err.Error())
+		}
+		db.Save(&user)
+		return c.JSON(user)
+
+	})
+	 
+
+	
+	
 	 app.Listen(":3000")
+
 
 	 
 
