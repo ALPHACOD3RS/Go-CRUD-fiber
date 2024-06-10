363143a82ff65bdd32082aeca6e8698394d84dcb
Naol Ketema <naolketema55@gmail.com>
2024-06-10 16:08:49 +0300

    user model done

--- models.go
+++ models.go
@@ -4,6 +4,15 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Name string 
-	Email string
+	Name string 	`json:"name" gorm:"not null"`
+	Email string	`json:"email" gorm:"unique; not null"`
+	Password string `json:"password" gorm:"not null"`
+	Posts []Post  	 `json:"posts" gorm:"foreignkey:UserID"`
+}
+
+type Post struct {
+	gorm.Model
+	Title   string     `json:"title"`
+	Content string	   `json:"content"`
+	UserID  string     `json:"user_id"`
 }
